4c43ff430b352590deb51d1e69c84ab9070642a1
Phatdanai Shinpanjapol <phatdanai.shin@kmutt.ac.th>
2022-05-09 16:51:13 +0700

    W2-05 Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import './App.css';
 import Main from './components/MainComponent';
 import { DISHES } from './shared/dishes';
 import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -19,8 +20,14 @@ class App extends Component {
   
   render() {
     return (
+      
       <div className="App">
-        <Main/>
+        <BrowserRouter>
+        <div className="App">
+        <Main />
+        </div>
+        </BrowserRouter>
+
       </div>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 
 
@@ -14,7 +21,10 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null,
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders: LEADERS
+
     };
   }
 
@@ -23,22 +33,37 @@ class Main extends Component {
   }
 
   render() {
+
+    const HomePage = () => {
+        return(
+            <Home 
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+            />
+  
+          
+      );
+    }
+
+
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <Footer />
-
         
+      <div>
+        <Header />
+        <Switch>
+              <Route path='/home' component={HomePage} />
+              <Route exact path='/contactus' component={Contact} />
+              <Redirect to="/home" />
+        </Switch>
+        <Menu dishes={this.state.dishes} selectedDish={this.state.selectedDish} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Footer />
 
       </div>
     );
+
+    
+    
   }
 }
 
